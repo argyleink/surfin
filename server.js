1744595995f09b25ca23c9368b0fe17c2597c74c
Adam Argyle <atom@argyleink.com>
2012-05-19 17:24:39 +0000

    server commit

---
+++ server.js
@@ -0,0 +1,79 @@
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path');
+
+// Database Connection //
+var db = "";
+
+// CREATE SERVER //
+http.createServer(function(request, response) {
+
+    if (request.headers['content-type'] == 'text/json') {
+        serveData(request, response);
+    }
+    else {
+        serveStaticFile(request, response);
+    }
+
+
+}).listen(process.env.PORT, "0.0.0.0" || 8001);
+console.log('server is =)');
+
+// FILE SERVER //
+var serveStaticFile = function(request, response) {
+        var filePath = '.' + request.url;
+        if (filePath == './') filePath = './index.html';
+
+
+        var extname = path.extname(filePath);
+        var contentType = 'text/html';
+        switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        }
+
+        console.log('serving ' + filePath + ", content type: " + contentType);
+
+        path.exists(filePath, function(exists) {
+
+            if (exists) {
+                fs.readFile(filePath, function(error, content) {
+                    if (error) {
+                        response.writeHead(500);
+                        response.end();
+                    }
+                    else {
+                        response.writeHead(200, {
+                            'Content-Type': contentType
+                        });
+                        response.end(content, 'utf-8');
+                    }
+                });
+            }
+            else {
+                response.writeHead(404);
+                response.end();
+            }
+        });
+    };
+
+// SERVICES //
+var serveData = function(req, res) {
+    console.log('data request: ' + req.url);
+
+    if (req.url == '/nerd-list') {
+        nerds.find({}).sort({
+            name: 1
+        }).toArray(function(err, items) {
+            if (err) throw err;
+
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(JSON.stringify(items));
+        });
+    }
+
+};
